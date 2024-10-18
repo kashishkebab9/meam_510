7697bafd7cd18747bd6cf58bd3cad62c9144cc99
Kashish Garg <kashishgarg9@gmail.com>
2024-10-17 20:08:26 -0400

    hard code and functionalized adc working

--- lab_03/src/main.c
+++ lab_03/src/main.c
@@ -6,6 +6,43 @@
 #include <stdint.h>
 #include "m_usb.h"
 
+void setupADC(uint8_t pin) {
+  clear(DDRF, pin);         
+  clear(PORTF, pin); 
+                   
+  clear(ADMUX, REFS1);
+  set(ADMUX, REFS0);
+
+  // clear the following to set the mux pins
+  clear(ADCSRA, ADEN);
+
+  // ADC Prescalar
+  set(ADCSRA, ADPS2);
+  set(ADCSRA, ADPS1);
+  set(ADCSRA, ADPS0);
+
+  // Disable Digital Input
+  switch (pin) {
+    case 0:
+      set(DIDR0, ADC0D);
+    case 1:
+      set(DIDR0, ADC1D);
+    case 2:
+      set(DIDR0, ADC2D);
+    case 3:
+      set(DIDR0, ADC3D);
+    case 4:
+      set(DIDR0, ADC4D);
+    case 5:
+      set(DIDR0, ADC5D);
+    case 6:
+      set(DIDR0, ADC6D);
+    case 7:
+      set(DIDR0, ADC7D);
+  }
+  // re-set this after config
+  set(ADCSRA, ADEN);
+}
 
 void setupADC_hardcode(){
   // Setup PF0 and PF1 for ADC
@@ -35,7 +72,7 @@ void setupADC_hardcode(){
 }
 
 uint16_t readADC(uint8_t channel) {
-  // Channel 0 hard code
+  // 
   clear(ADMUX, MUX5);
   clear(ADMUX, MUX2);
   clear(ADMUX, MUX1);
@@ -56,7 +93,9 @@ int main(void)
 {
 
     m_usb_init();
-    setupADC_hardcode();
+    // setupADC_hardcode();
+    setupADC(0);
+    setupADC(4);
     _clockdivide(0); 
 
 
@@ -66,7 +105,7 @@ int main(void)
       m_usb_tx_uint(val_x);
       m_usb_tx_string("\n");
 
-      uint16_t val_y = readADC(1);
+      uint16_t val_y = readADC(4);
       m_usb_tx_string("V_Ry: ");
       m_usb_tx_uint(val_y);
       m_usb_tx_string("\n");
