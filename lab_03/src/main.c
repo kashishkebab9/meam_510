ccc4344b6714ca9536946ef86b50edf7738743e5
Kashish Garg <kashishgarg9@gmail.com>
2024-10-20 17:53:53 -0400

    Working dual motor i/o

--- lab_03/src/main.c
+++ lab_03/src/main.c
@@ -4,7 +4,14 @@
 
 #include "MEAM_general.h"  // includes the resources included in the MEAM_general.h file
 #include <stdint.h>
+#include <stdio.h>
 #include "m_usb.h"
+#define SERVO_MIN 8 // The minimum value we can set OCR3A with the timer config we have
+#define SERVO_MAX 39 // The maximum value we can set OCR3A with the timer config we have
+#define SERVO_RANGE SERVO_MAX - SERVO_MIN // The maximum value we can set OCR3A with the timer config we have
+#define JOY_MIN -1024
+#define JOY_MAX 1024
+#define JOY_RANGE JOY_MAX - JOY_MIN
 
 void setupADC(uint8_t pin) {
   clear(DDRF, pin);         
@@ -62,9 +69,32 @@ uint16_t readADC(uint8_t channel) {
   return val;
 }
 
+uint16_t joystick_to_pwm(uint16_t joy_val) {
+  m_usb_tx_string("JTP: \n");
+
+  joy_val += abs(JOY_MIN);
+  m_usb_tx_uint(joy_val);
+  m_usb_tx_string("\n");
+  m_usb_tx_uint(SERVO_RANGE);
+  m_usb_tx_string("\n");
+  float joy_percent = (float)joy_val / 2048.0;
+  // m_usb_tx_string("\n");
+  joy_val = (uint16_t)(joy_percent * SERVO_RANGE);
+
+  joy_val += SERVO_MIN ;
+  m_usb_tx_uint(joy_val);
+  m_usb_tx_string("\n");
+  // joy_val += SERVO_MIN;
+  // m_usb_tx_uint(joy_val);
+  // m_usb_tx_string("\n");
+  // m_usb_tx_string("===========\n");
+  return joy_val;
+
+}
+
 int main(void)
 {
-    //TIMER SETUP
+    //TIMER SETUP - OC3A
     _clockdivide(0); //set the clock speed to 16Mhz
     set(DDRC,6);        // Allow pin 6 to be output
 
@@ -86,8 +116,24 @@ int main(void)
     set(TCCR3B, WGM32);
     clear(TCCR3A, WGM31);
     set(TCCR3A, WGM30);
-    OCR3A = 8;
     //TIMER SETUP
+    //TIMER SETUP - OC4A
+    set(DDRD,0);  
+
+    // Same prescalar as the other timer
+    set(TCCR0B, CS02);
+    clear(TCCR0B, CS01);
+    set(TCCR0B, CS00);
+
+    // Same output compare as other timer
+    set(TCCR0A, COM0B1);  
+    clear(TCCR0A, COM0B0);  
+
+
+    clear(TCCR0B, WGM02);
+    set(TCCR0A, WGM01);
+    set(TCCR0A, WGM00);
+
 
 
     m_usb_init();
@@ -108,7 +154,8 @@ int main(void)
       m_usb_tx_string("V_Ry: ");
       m_usb_tx_uint(val_y);
       m_usb_tx_string("\n");
-
+      OCR3A = joystick_to_pwm(val_x);
+      OCR0B = joystick_to_pwm(val_y);
     }
 
     return 0;   /* never reached */
